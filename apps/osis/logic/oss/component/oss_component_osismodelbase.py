74e3f792717374cf2ac5fe7ceeffdb933eb0db5a
despiegk <kristof@incubaid.com>
2014-10-19 09:08:46 +0200

    new processmanager

---
+++ apps/osis/logic/oss/component/oss_component_osismodelbase.py
@@ -0,0 +1,228 @@
+from JumpScale import j
+
+class oss_component_osismodelbase(j.code.classGetJSRootModelBase()):
+    def __init__(self):
+        self._P_type=""
+    
+        self._P_nr=0
+    
+        self._P_brand=""
+    
+        self._P_model=""
+    
+        self._P_description=""
+    
+        self._P_supportremarks=""
+    
+        self._P_comments=list()
+    
+        self._P_id=0
+    
+        self._P_guid=""
+    
+        self._P__meta=list()
+    
+        self._P__meta=["osismodel","oss","component",1] #@todo version not implemented now, just already foreseen
+    
+
+        pass
+
+    @property
+    def type(self):
+        return self._P_type
+    @type.setter
+    def type(self, value):
+        
+        if not isinstance(value, str) and value is not None:
+            if isinstance(value, basestring) and j.basetype.string.checkString(value):
+                value = j.basetype.string.fromString(value)
+            else:
+                msg="property type input error, needs to be str, specfile: /opt/jumpscale/apps/osis/logic/oss/model.spec, name model: component, value was:" + str(value)
+                raise RuntimeError(msg)
+    
+
+        self._P_type=value
+    @type.deleter
+    def type(self):
+        del self._P_type
+
+
+    @property
+    def nr(self):
+        return self._P_nr
+    @nr.setter
+    def nr(self, value):
+        
+        if not isinstance(value, int) and value is not None:
+            if isinstance(value, basestring) and j.basetype.integer.checkString(value):
+                value = j.basetype.integer.fromString(value)
+            else:
+                msg="property nr input error, needs to be int, specfile: /opt/jumpscale/apps/osis/logic/oss/model.spec, name model: component, value was:" + str(value)
+                raise RuntimeError(msg)
+    
+
+        self._P_nr=value
+    @nr.deleter
+    def nr(self):
+        del self._P_nr
+
+
+    @property
+    def brand(self):
+        return self._P_brand
+    @brand.setter
+    def brand(self, value):
+        
+        if not isinstance(value, str) and value is not None:
+            if isinstance(value, basestring) and j.basetype.string.checkString(value):
+                value = j.basetype.string.fromString(value)
+            else:
+                msg="property brand input error, needs to be str, specfile: /opt/jumpscale/apps/osis/logic/oss/model.spec, name model: component, value was:" + str(value)
+                raise RuntimeError(msg)
+    
+
+        self._P_brand=value
+    @brand.deleter
+    def brand(self):
+        del self._P_brand
+
+
+    @property
+    def model(self):
+        return self._P_model
+    @model.setter
+    def model(self, value):
+        
+        if not isinstance(value, str) and value is not None:
+            if isinstance(value, basestring) and j.basetype.string.checkString(value):
+                value = j.basetype.string.fromString(value)
+            else:
+                msg="property model input error, needs to be str, specfile: /opt/jumpscale/apps/osis/logic/oss/model.spec, name model: component, value was:" + str(value)
+                raise RuntimeError(msg)
+    
+
+        self._P_model=value
+    @model.deleter
+    def model(self):
+        del self._P_model
+
+
+    @property
+    def description(self):
+        return self._P_description
+    @description.setter
+    def description(self, value):
+        
+        if not isinstance(value, str) and value is not None:
+            if isinstance(value, basestring) and j.basetype.string.checkString(value):
+                value = j.basetype.string.fromString(value)
+            else:
+                msg="property description input error, needs to be str, specfile: /opt/jumpscale/apps/osis/logic/oss/model.spec, name model: component, value was:" + str(value)
+                raise RuntimeError(msg)
+    
+
+        self._P_description=value
+    @description.deleter
+    def description(self):
+        del self._P_description
+
+
+    @property
+    def supportremarks(self):
+        return self._P_supportremarks
+    @supportremarks.setter
+    def supportremarks(self, value):
+        
+        if not isinstance(value, str) and value is not None:
+            if isinstance(value, basestring) and j.basetype.string.checkString(value):
+                value = j.basetype.string.fromString(value)
+            else:
+                msg="property supportremarks input error, needs to be str, specfile: /opt/jumpscale/apps/osis/logic/oss/model.spec, name model: component, value was:" + str(value)
+                raise RuntimeError(msg)
+    
+
+        self._P_supportremarks=value
+    @supportremarks.deleter
+    def supportremarks(self):
+        del self._P_supportremarks
+
+
+    @property
+    def comments(self):
+        return self._P_comments
+    @comments.setter
+    def comments(self, value):
+        
+        if not isinstance(value, list) and value is not None:
+            if isinstance(value, basestring) and j.basetype.list.checkString(value):
+                value = j.basetype.list.fromString(value)
+            else:
+                msg="property comments input error, needs to be list, specfile: /opt/jumpscale/apps/osis/logic/oss/model.spec, name model: component, value was:" + str(value)
+                raise RuntimeError(msg)
+    
+
+        self._P_comments=value
+    @comments.deleter
+    def comments(self):
+        del self._P_comments
+
+
+    @property
+    def id(self):
+        return self._P_id
+    @id.setter
+    def id(self, value):
+        
+        if not isinstance(value, int) and value is not None:
+            if isinstance(value, basestring) and j.basetype.integer.checkString(value):
+                value = j.basetype.integer.fromString(value)
+            else:
+                msg="property id input error, needs to be int, specfile: /opt/jumpscale/apps/osis/logic/oss/model.spec, name model: component, value was:" + str(value)
+                raise RuntimeError(msg)
+    
+
+        self._P_id=value
+    @id.deleter
+    def id(self):
+        del self._P_id
+
+
+    @property
+    def guid(self):
+        return self._P_guid
+    @guid.setter
+    def guid(self, value):
+        
+        if not isinstance(value, str) and value is not None:
+            if isinstance(value, basestring) and j.basetype.string.checkString(value):
+                value = j.basetype.string.fromString(value)
+            else:
+                msg="property guid input error, needs to be str, specfile: /opt/jumpscale/apps/osis/logic/oss/model.spec, name model: component, value was:" + str(value)
+                raise RuntimeError(msg)
+    
+
+        self._P_guid=value
+    @guid.deleter
+    def guid(self):
+        del self._P_guid
+
+
+    @property
+    def _meta(self):
+        return self._P__meta
+    @_meta.setter
+    def _meta(self, value):
+        
+        if not isinstance(value, list) and value is not None:
+            if isinstance(value, basestring) and j.basetype.list.checkString(value):
+                value = j.basetype.list.fromString(value)
+            else:
+                msg="property _meta input error, needs to be list, specfile: /opt/jumpscale/apps/osis/logic/oss/model.spec, name model: component, value was:" + str(value)
+                raise RuntimeError(msg)
+    
+
+        self._P__meta=value
+    @_meta.deleter
+    def _meta(self):
+        del self._P__meta
+
--- apps/osis/logic/oss/user/oss_user_osismodelbase.py
+++ apps/osis/logic/oss/user/oss_user_osismodelbase.py
@@ -8,15 +8,17 @@ class oss_user_osismodelbase(j.code.classGetJSRootModelBase()):
     def __init__(self):
         self._P_id=0
     
-        self._P_organization=""
+        self._P_list(str)=""
     
-        self._P_organization_name=""
+        self._P_organization_names=""
     
         self._P_name=""
     
         self._P_addresses=list()
     
         self._P_comments=list()
+    
+        self._P_userids=list()
     
         self._P_contactmethods=list()
     
@@ -54,43 +56,43 @@ class oss_user_osismodelbase(j.code.classGetJSRootModelBase()):
 
 
     @property
-    def organization(self):
-        return self._P_organization
-    @organization.setter
-    def organization(self, value):
+    def list(str)(self):
+        return self._P_list(str)
+    @list(str).setter
+    def list(str)(self, value):
         
         if not isinstance(value, str) and value is not None:
             if isinstance(value, basestring) and j.basetype.string.checkString(value):
                 value = j.basetype.string.fromString(value)
             else:
-                msg="property organization input error, needs to be str, specfile: /opt/jumpscale/apps/osis/logic/oss/model.spec, name model: user, value was:" + str(value)
+                msg="property list(str) input error, needs to be str, specfile: /opt/jumpscale/apps/osis/logic/oss/model.spec, name model: user, value was:" + str(value)
                 raise RuntimeError(msg)
     
 
-        self._P_organization=value
-    @organization.deleter
-    def organization(self):
-        del self._P_organization
+        self._P_list(str)=value
+    @list(str).deleter
+    def list(str)(self):
+        del self._P_list(str)
 
 
     @property
-    def organization_name(self):
-        return self._P_organization_name
-    @organization_name.setter
-    def organization_name(self, value):
+    def organization_names(self):
+        return self._P_organization_names
+    @organization_names.setter
+    def organization_names(self, value):
         
         if not isinstance(value, str) and value is not None:
             if isinstance(value, basestring) and j.basetype.string.checkString(value):
                 value = j.basetype.string.fromString(value)
             else:
-                msg="property organization_name input error, needs to be str, specfile: /opt/jumpscale/apps/osis/logic/oss/model.spec, name model: user, value was:" + str(value)
+                msg="property organization_names input error, needs to be str, specfile: /opt/jumpscale/apps/osis/logic/oss/model.spec, name model: user, value was:" + str(value)
                 raise RuntimeError(msg)
     
 
-        self._P_organization_name=value
-    @organization_name.deleter
-    def organization_name(self):
-        del self._P_organization_name
+        self._P_organization_names=value
+    @organization_names.deleter
+    def organization_names(self):
+        del self._P_organization_names
 
 
     @property
@@ -151,6 +153,26 @@ class oss_user_osismodelbase(j.code.classGetJSRootModelBase()):
     @comments.deleter
     def comments(self):
         del self._P_comments
+
+
+    @property
+    def userids(self):
+        return self._P_userids
+    @userids.setter
+    def userids(self, value):
+        
+        if not isinstance(value, list) and value is not None:
+            if isinstance(value, basestring) and j.basetype.list.checkString(value):
+                value = j.basetype.list.fromString(value)
+            else:
+                msg="property userids input error, needs to be list, specfile: /opt/jumpscale/apps/osis/logic/oss/model.spec, name model: user, value was:" + str(value)
+                raise RuntimeError(msg)
+    
+
+        self._P_userids=value
+    @userids.deleter
+    def userids(self):
+        del self._P_userids
 
 
     @property
@@ -252,17 +274,3 @@ class oss_user_osismodelbase(j.code.classGetJSRootModelBase()):
     def _meta(self):
         del self._P__meta
 
-
-    def new_datasource(self,value=None):
-
-        if value==None:
-            value2=j.core.codegenerator.getClassJSModel("osismodel","oss","datasource")()
-        else:
-            value2=value
-        
-        self._P_datasources.append(value2)
-        if self._P_datasources[-1].__dict__.has_key("_P_id"):
-            self._P_datasources[-1].id=len(self._P_datasources)
-        return self._P_datasources[-1]
-        
-    
